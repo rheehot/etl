42d4b631df77ac7be9e7d619d8e548cdba8c0fc1
wizmusa <wizmusa@gmail.com>
2017-08-31 15:38:13 +0900

    Update delete_newline.py
    SOH 문자 관련 주석 입력 및 행 순서 보완

--- utility/delete_newline.py
+++ utility/delete_newline.py
@@ -1,13 +1,15 @@
 '''
-delete_newline.py ‘데이터파일명’ ‘줄 별 구분자 개수’ ‘구분자’ > 출력파일명'''
+delete_newline.py ‘데이터파일명’ ‘줄 별 구분자 개수’ ‘구분자’ > 출력파일명
+※ SOH 구분자로 입력하려면 커맨드라인에서 [Ctrl]+[v]+[a] 입력
+'''
 
 import sys
 reload(sys)
 
 f = open(sys.argv[1], 'r')
-delimiter = '\x01' # 구분자: SOH 문자
-# delimiter = sys.argv[3] # command line에서 입력
 delimiter_count = int(sys.argv[2])
+delimiter = sys.argv[3] # command line에서 입력
+# delimiter = '\x01' # 구분자: SOH 문자
 count = 0
 temp_string = ''
 
@@ -35,6 +37,5 @@ while True:
             line = line.replace('\n', '')
             temp_string += line
 #while
-
 
 f.close()
